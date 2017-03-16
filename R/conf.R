b92d37503c5037b2ab40b3e2fb0fd2c6883a0006
Douglas Ashton <douglas.j.ashton@gmail.com>
2017-03-16 20:58:34 +0000

    solr.url updated to solr hostname
    This lines up with the docker-compose configuration used for testing

--- R/conf.R
+++ R/conf.R
@@ -2,7 +2,7 @@
 ## configuration environment -- loaded from the config file in configure.rcloud()
 .rc.conf <- new.env(parent=emptyenv())
 
-.rc.conf$solr.url <- "http://localhost:8983/solr/rcloudnotebooks"
+.rc.conf$solr.url <- "http://solr:8983/solr/rcloudnotebooks"
 .rc.conf$solr.auth.user <- NULL
 .rc.conf$solr.auth.pwd <- NULL
 
