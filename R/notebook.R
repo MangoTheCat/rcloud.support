d7adbc84e28a7e604ce3720dfae061ca959b6888
Doug Ashton <douglas.j.ashton@gmail.com>
2017-09-01 12:31:01 +0100

    add various notebook functions

--- R/notebook.R
+++ R/notebook.R
@@ -1,3 +1,11 @@
 rcloud.is.notebook.visible <- function(id) TRUE
 
 is.notebook.encrypted <- function(id) FALSE
+
+rcloud.get.notebook <- function(id) NULL
+
+rcloud.notebook.star.count <- function(id) 0
+
+rcloud.get.users <- function() "rstudio"
+
+rcloud.config.all.notebooks.multiple.users <- function(users) NULL
